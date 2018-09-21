0141e245061a546ee6c146bfd099fd141564e84d
Fernando <fernandovieiraf02@gmail.com>
2018-09-21 17:41:15 -0400

    Correção de erro na thread RefleshUI

--- app/src/main/java/com/whatsappclone/whatsappclone/whatsappclone/threads/SendMessage.java
+++ app/src/main/java/com/whatsappclone/whatsappclone/whatsappclone/threads/SendMessage.java
@@ -1,16 +1,15 @@
 package com.whatsappclone.whatsappclone.whatsappclone.threads;
 
-import com.whatsappclone.whatsappclone.whatsappclone.activity.MainActivity;
 import com.whatsappclone.whatsappclone.whatsappclone.fragment.ChatFragment;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class SendMessage implements Runnable {
 
@@ -18,12 +17,19 @@ public class SendMessage implements Runnable {
     private static final String SERVER_IP = "192.168.2.88";
     private ArrayList<String> payloadList;
     private Socket socket;
-    private DataInputStream is;
+    private InputMessage inputTask;
+    private RefleshUI refleshTask;
     private DataOutputStream os;
     private BufferedReader buffer;
+    private Semaphore semaphore;
 
     public SendMessage() {
         payloadList = new ArrayList<>();
+        os = null;
+        inputTask = null;
+        buffer = null;
+        refleshTask = new RefleshUI();
+        semaphore = new Semaphore(1);
     }
 
     public ArrayList<String> getPayloadList() {
@@ -34,23 +40,27 @@ public class SendMessage implements Runnable {
         this.payloadList.add(payload);
     }
 
+    private void refleshUI() {
+        ChatFragment.fragmentActivity.runOnUiThread(refleshTask);
+    }
+
     @Override
     public void run() {
         try {
             socket = new Socket(SERVER_IP, SERVERPORT);
-            os = null;
-            is = null;
-            buffer = null;
             os = new DataOutputStream(socket.getOutputStream());
-            //is = new DataInputStream(socket.getInputStream());
-        } catch(IOException e) {
+            inputTask = new InputMessage(socket);
+            new Thread(inputTask).start();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
         String message = null;
         int sizeList = 0;
         int index = 0;
+
         try {
-            while(true) {
+             while(true) {
                 sizeList = payloadList.size();
                 if(index < sizeList) {
                     message = payloadList.get(index);
@@ -59,47 +69,49 @@ public class SendMessage implements Runnable {
                         if (message != null) {
                             os.writeBytes(message);
                             os.flush();
-
-                            new Thread(new Runnable() {
-                                @Override
-                                public void run() {
-                                try {
-                                    buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                                    while (true) {
-                                        if (buffer.ready()) {
-                                            ChatFragment.chatList.add(buffer.readLine());
-                                            ChatFragment.fragmentActivity.runOnUiThread(new Runnable() {
-                                                @Override
-                                                public void run() {
-                                                    ChatFragment.chatAdapter.notifyDataSetChanged();
-                                                }
-                                            });
-                                            break;
-                                        }
-                                    }
-                                } catch (IOException e) {
-                                    // TODO Auto-generated catch block
-                                    e.printStackTrace();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                                }
-                            }).start();
-
-                            /*getActivity().runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    chatAdapter.notifyDataSetChanged();
-                                }
-                            });*/
                         }
                     }
+                } else if (sizeList == 0) {
+                    index = 0;
                 }
             }
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private class RefleshUI implements Runnable {
+        @Override
+        public void run() {
+            ChatFragment.chatAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private class InputMessage implements Runnable {
+        private Socket socket;
+
+        public InputMessage(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while (true) {
+                    if (buffer.ready()) {
+                        ChatFragment.chatList.add(buffer.readLine());
+                        refleshUI();
+                    }
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- server-nodejs/index.js
+++ server-nodejs/index.js
@@ -7,16 +7,18 @@ const server = Net.createServer();
 server.listen(3030, () => {
     console.log('Server Listenning on:', server.address());
 });
+
 server.on('connection', sock => {
     console.log('Client connected:', sock.address());
     sockets.push(sock);
 
     sock.on('data', data => {
         console.log('client message:', data.toString());
-        for(let item of sockets) {
-            if(item != sock) {
-                console.log(item.address())
-                item.write(data.toString());
+        if(sockets.length > 0) {
+            for(let item of sockets) {
+                if(item !== sock) {
+                    item.write(data.toString());
+                }
             }
         }
     });
